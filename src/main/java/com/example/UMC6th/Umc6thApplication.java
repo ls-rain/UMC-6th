35d5ed8e70750725a1007f506b8a07a2a13e17c3
ls-rain <swlee19990115@gmail.com>
2024-05-09 16:55:47 +0900

    Initial commit

---
+++ src/main/java/com/example/UMC6th/Umc6thApplication.java
@@ -0,0 +1,13 @@
+package com.example.UMC6th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc6thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umc6thApplication.class, args);
+	}
+
+}
